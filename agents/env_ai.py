f7af3633cece004c9457c17aefb827080fa536c1
db6jzfc2fp-arch <db6jzfc2fp@privaterelay.appleid.com>
2026-06-29 16:49:59 +0900

    Create env_ai.py

---
+++ agents/env_ai.py
@@ -0,0 +1,11 @@
+class EnvAI:
+
+    def analyze_environment_data(self, df):
+
+        return {
+            "temperature": 25,
+            "humidity": 72,
+            "risk": "낮음",
+            "score": 92,
+            "recommendation": "현재 환경은 안정적입니다."
+        }
