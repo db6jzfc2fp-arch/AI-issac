910c8629e76b409d583c926fec7a85e5ba6b77b4
db6jzfc2fp-arch <db6jzfc2fp@privaterelay.appleid.com>
2026-06-29 20:58:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -157,22 +157,22 @@ if analyze:
 
     st.subheader("🌡️ Env-AI 환경 분석")
 
-st.metric(
-    "환경 위험도",
-    f"{env_result['risk_score']}점"
-)
+    st.metric(
+        "환경 위험도",
+        f"{env_result['risk_score']}점"
+    )
 
-st.write("위험등급 :", env_result["risk_level"])
+    st.write("위험등급 :", env_result["risk_level"])
 
-st.write("### 분석 이유")
+    st.write("### 분석 이유")
 
-for reason in env_result["reasons"]:
-    st.write("-", reason)
+    for reason in env_result["reasons"]:
+        st.write("-", reason)
 
     st.write("### 개선 방법")
 
-for tip in env_result["advice"]:
-    st.write("-", tip)
+    for tip in env_result["advice"]:
+        st.write("-", tip)
 
     # AI 연구원 실제 실행
     env_ai = EnvAI()
